a177fff096daae68a0b26240a940b4197d5c4a5e
Iqbal Choirul Rasyid <iqbalchoirulrasyid@gmail.com>
2025-12-16 23:02:18 +0700

    pengerjaan model

--- Coretan/coba.py
+++ Coretan/coba.py
@@ -1,1 +1,53 @@
-print("kel 4")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+file_path = "X:\Code\Tubes_Phyton_Kel4\Dataset\cars.xlsx"
+data_df = pd.read_excel(file_path)
+
+
+median_mpg = data_df['MPG_City'].median()
+data_df['Irit_Boros_Binary'] = np.where(data_df['MPG_City'] >= median_mpg, 1, 0)
+
+features_X = ['Horsepower', 'Weight', 'Cylinders']
+target_Y = 'Irit_Boros_Binary'
+
+data_df.dropna(subset=features_X + [target_Y], inplace=True)
+
+class CustomDataset:
+    def __init__(self, mobil, features, target_name):
+
+        self.data = mobil
+        self.target = mobil[target_name]
+        self.feature_names = features
+        self.target_name = target_name
+        self.features = mobil[features] 
+
+dataset = CustomDataset(
+    mobil=data_df,
+    features=features_X,
+    target_name=target_Y
+)
+
+X = dataset.features  
+Y = dataset.target    
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size= 0.2, random_state= 42
+)
+
+model = LogisticRegression(solver='liblinear') 
+model.fit(X_train, Y_train)
+
+
+Y_pred = model.predict(X_test)
+accuracy = accuracy_score(Y_test, Y_pred)
+
+print(f"Features (X):\n{X.head()}")
+print(f"Target (Y):\n{Y.head()}")
+print(f"\nModel Accuracy: {accuracy:.2f}")
